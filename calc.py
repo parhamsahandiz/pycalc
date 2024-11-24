082c116a05fd335ddac9a429587ae1be47632a77
parham.sahandi <parham.sahandi@gmail.com>
2024-11-25 10:54:15 +1100

    added support for subtraction

--- calc.py
+++ calc.py
@@ -3,9 +3,11 @@ def compute(expression):
     num0 = int(values[0])
     operator = values[1]
     num1 = int(values[2])
-    if operator == '+':
-        return num0 + num1
+    elif operator
+    if operator == '-':
+        return num0 - num1
     else:
         print('unknown operator!')
         return None
+  
     
